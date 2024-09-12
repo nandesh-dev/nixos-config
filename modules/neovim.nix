1f54deb2e7ce840c9745248e680d2e2e622ef876
nandesh-dev <nandesh.dev@gmail.com>
2024-09-12 06:14:42 +0000

    Modify Neovim config to disable arrow keys

--- modules/neovim.nix
+++ modules/neovim.nix
@@ -32,6 +32,10 @@
                 vim.cmd("filetype plugin indent on")
 
                 vim.api.nvim_set_keymap("n", "<C-s>", ":w<CR>", { noremap = true, silent = true })
+                vim.api.nvim_set_keymap("", "<Up>", "<NOP>", { noremap = true, silent = true })
+                vim.api.nvim_set_keymap("", "<Down>", "<NOP>", { noremap = true, silent = true })
+                vim.api.nvim_set_keymap("", "<Left>", "<NOP>", { noremap = true, silent = true })
+                vim.api.nvim_set_keymap("", "<Right>", "<NOP>", { noremap = true, silent = true })
               '';
             }
             {

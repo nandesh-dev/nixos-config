0b635d5e5055a053ea009c8b487d26746484c56b
nandesh-dev <nandesh.dev@gmail.com>
2024-09-12 06:13:56 +0000

    Modify Neovim config to add keybinds for cmp

--- modules/neovim.nix
+++ modules/neovim.nix
@@ -297,6 +297,8 @@
                 	},
                 	window = {},
                 	mapping = cmp.mapping.preset.insert({
+                    ['<C-j>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+                    ['<C-k>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
                 		["<C-b>"] = cmp.mapping.scroll_docs(-4),
                 		["<C-f>"] = cmp.mapping.scroll_docs(4),
                 		["<C-Space>"] = cmp.mapping.complete(),

10f145205fdd21f9b1c8815d2faf0c14af6c341d
nandesh-dev <nandesh.dev@gmail.com>
2025-02-07 08:37:08 +0000

    Remove theme and supar dark mode plugin from brave

--- modules/brave.nix
+++ modules/brave.nix
@@ -7,10 +7,8 @@
         enable = true;
         package = pkgs.brave;
         extensions = [
-          { id = "ihennfdbghdiflogeancnalflhgmanop"; } # gruvbox theme
           { id = "nngceckbapebfimnlniiiahkandclblb"; } # bitwarden password manager
           { id = "nffaoalbilbmmfgbnbgppjihopabppdk"; } # video speed controller
-          { id = "nlgphodeccebbcnkgmokeegopgpnjfkc"; } # super dark mode
         ];
         commandLineArgs = [ "--force-dark-mode" ];
       };

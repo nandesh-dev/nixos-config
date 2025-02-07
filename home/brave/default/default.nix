e4f0a05f6b5e1b01731abfd4fa70156d5382556b
nandesh-dev <nandesh.dev@gmail.com>
2025-02-07 19:26:48 +0000

    Add catppuccin frappe theme to brave

--- home/brave/default/default.nix
+++ home/brave/default/default.nix
@@ -6,6 +6,7 @@
     extensions = [
       { id = "nngceckbapebfimnlniiiahkandclblb"; } # bitwarden password manager
       { id = "nffaoalbilbmmfgbnbgppjihopabppdk"; } # video speed controller
+      { id = "olhelnoplefjdmncknfphenjclimckaf"; } # catppuccin color theme
     ];
     commandLineArgs = [ "--force-dark-mode" ];
   };

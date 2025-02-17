6b9958da4ddd90b387b7a69dd688b75bd86f7d2e
nandesh-dev <nandesh.dev@gmail.com>
2025-02-17 17:14:48 +0000

    Switch to power saving config for cpu perf

--- system/tlp/default/default.nix
+++ system/tlp/default/default.nix
@@ -4,11 +4,11 @@
     enable = true;
 
     settings = {
-      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_AC = "powersave";
       CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
       CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "power";
 
       PLATFORM_PROFILE_ON_AC = "balanced";
       PLATFORM_PROFILE_ON_BAT = "low-power";

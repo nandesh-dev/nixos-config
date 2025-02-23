2aed91fb5dbe2c8adda3f574bf11ccee4c785691
nandesh-dev <nandesh.dev@gmail.com>
2025-02-23 09:04:39 +0000

    Switch to performance profile for tlp

--- system/tlp/default/default.nix
+++ system/tlp/default/default.nix
@@ -10,7 +10,7 @@
       CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
       CPU_ENERGY_PERF_POLICY_ON_AC = "power";
 
-      PLATFORM_PROFILE_ON_AC = "balanced";
+      PLATFORM_PROFILE_ON_AC = "performance";
       PLATFORM_PROFILE_ON_BAT = "low-power";
 
       START_CHARGE_THRESH_BAT0 = 0;

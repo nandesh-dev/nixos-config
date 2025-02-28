37465acc73660a5f86631f90355ce731678942ce
nandesh-dev <nandesh.dev@gmail.com>
2025-02-28 09:16:05 +0000

    Revert tlp processor to performance mode

--- system/tlp/default/default.nix
+++ system/tlp/default/default.nix
@@ -4,7 +4,7 @@
     enable = true;
 
     settings = {
-      CPU_SCALING_GOVERNOR_ON_AC = "powersave";
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
       CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
       CPU_ENERGY_PERF_POLICY_ON_BAT = "power";

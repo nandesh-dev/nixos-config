b519eba3bcd6a5ccfd3c87d0cd0b8f63fe6e6864
nandesh-dev <nandesh.dev@gmail.com>
2025-04-27 15:34:35 +0000

    Switch to grub for bootloader

--- system/boot_loader/default/default.nix
+++ system/boot_loader/default/default.nix
@@ -1,5 +1,10 @@
 { ... }:
 {
-  boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub = {
+    enable = true;
+    useOSProber = true;
+    efiSupport = true;
+    device = "nodev";
+  };
 }

e6338d2319bca6984ad4a1cefe789abb9b206f28
nandesh-dev <nandesh.dev@gmail.com>
2026-05-22 14:56:57 +0000

    fix/system: invalid system configuration

--- configurations/dekstop.nix
+++ configurations/dekstop.nix
@@ -1,8 +1,6 @@
 {
   self,
-  pkgs,
   inputs,
-  config,
   ...
 }:
 {
@@ -13,38 +11,42 @@
       self.nixosModules.network
       self.nixosModules.preferences
       self.nixosModules.preservation
+      (
+        { pkgs, config, ... }:
+        {
+          users.users.nandesh = {
+            isNormalUser = true;
+            initialPassword = "1234";
+            extraGroups = [ "wheel" ];
+            packages = with pkgs; [
+              tree
+            ];
+          };
+
+          environment.systemPackages = with pkgs; [
+            vim
+            neovim
+          ];
+
+          programs.niri.enable = true;
+
+          services.greetd = {
+            enable = true;
+            settings = {
+              default_session = {
+                command = "${config.programs.niri.package}/bin/niri-session";
+                user = "nandesh";
+              };
+            };
+          };
+
+          systemd.user.services.niri.enableDefaultPath = false;
+
+          services.openssh.enable = true;
+
+          system.stateVersion = "25.11";
+        }
+      )
     ];
-
-    users.users.nandesh = {
-      isNormalUser = true;
-      initialPassword = "1234";
-      extraGroups = [ "wheel" ];
-      packages = with pkgs; [
-        tree
-      ];
-    };
-
-    environment.systemPackages = with pkgs; [
-      vim
-      neovim
-    ];
-
-    programs.niri.enable = true;
-
-    services.greetd = {
-      enable = true;
-      settings = {
-        default_session = {
-          command = "${config.programs.niri.package}/bin/niri-session";
-          user = "nandesh";
-        };
-      };
-    };
-
-    systemd.user.services.niri.enableDefaultPath = false;
-
-    services.openssh.enable = true;
-
-    system.stateVersion = "25.11";
   };
 }

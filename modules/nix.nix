130c069ff86702923969a7798bd01cc303341e51
nandesh-dev <nandesh.dev@gmail.com>
2026-05-23 12:56:20 +0000

    feat/nix: enable nix-command and flakes command

--- modules/nix.nix
+++ modules/nix.nix
@@ -12,6 +12,11 @@
         trusted-public-keys = [
           "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         ];
+
+        experimental-features = [
+          "nix-command"
+          "flakes"
+        ];
       };
     };
 }
